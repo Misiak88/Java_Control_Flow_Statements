4f0d5e90d08c6511835f0eac08000f260c4bdd99
Michał Kamiński <kaminski.mail@gmail.com>
2021-03-16 19:12:57 +0100

    Initial commit

---
+++ src/com/michas/Main.java
@@ -0,0 +1,13 @@
+package com.michas;
+
+public class Main {
+
+    public static void main(String[] args) {
+     ////   NumberInWord
+	 //NumberInWord.printNumberInWord(9);
+	 ////   NumberOfDaysInMonth
+	 //System.out.println(NumberOfDaysInMonth.isLeapYear(2001));
+	 //System.out.println(NumberOfDaysInMonth.getDaysInMonth(2,2000));
+
+    }
+}
